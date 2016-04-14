3936b006d249587295966b1e32fd92c446741413
Justin Huang <JCHuang@student.naperville203.org>
2016-04-14 15:09:41 -0500

    Capstone Cryptogram Scanner 4/14/16

--- Cryptogram/Cryptogram.java
+++ Cryptogram/Cryptogram.java
@@ -9,6 +9,7 @@ public class Cryptogram
     private static final String LBRAC = "([{<";
     private static final String RBRAC = ">}])";
     private static final String SYM = "@#$%^&";
+    private static final String ORDER = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.,!?([{<>}])@#$%^&";
     
     public static String encode(String message)
     {
@@ -28,7 +29,7 @@ public class Cryptogram
         
         for (int i = 0; i < message.length(); i++)
         {
-            
+            code += LCASE.charAt(LCASE.indexOf(message.charAt(i)) - 1);
         }
         
         return message;
@@ -37,7 +38,31 @@ public class Cryptogram
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
+        String choice = "";
         
-        System.out.print("Encode or decode?");
+        while (choice.equals("encode") == false && choice.equals("decode") == false)
+        {
+            System.out.print("Encode or decode? ");
+            choice = scanner.next().toLowerCase();
+        }
+        
+        scanner.useDelimiter("\n");
+        
+        if (choice.toLowerCase().equals("encode"))
+        {
+            System.out.print("Message to encode: ");
+            String message = scanner.next();
+            System.out.println(Cryptogram.encode(message));
+        }
+        else if (choice.toLowerCase().equals("decode"))
+        {
+            String code = "";
+            System.out.print("Code to decode: ");
+            while (scanner.hasNext())
+            {
+                code += scanner.next();
+            }
+            System.out.println(Cryptogram.decode(code));
+        }
     }
 }
