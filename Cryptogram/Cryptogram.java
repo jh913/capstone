cc6972dfd479e1a514c5a286a3513446ddac5593
Justin Huang <JCHuang@student.naperville203.org>
2016-04-15 15:08:05 -0500

    Fixed Scanner Token Reading 4/15/16

--- Cryptogram/Cryptogram.java
+++ Cryptogram/Cryptogram.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class Cryptogram
 {
-    private static final String LCASE = "abcdefghijklmnopqrstuvwxyz";
-    private static final String UCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final String NUM = "1234567890";
-    private static final String PUNC = ".,!?";
-    private static final String LBRAC = "([{<";
-    private static final String RBRAC = ">}])";
-    private static final String SYM = "@#$%^&";
+    //private static final String LCASE = "abcdefghijklmnopqrstuvwxyz";
+    //private static final String UCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //private static final String NUM = "1234567890";
+    //private static final String PUNC = ".,!?";
+    //private static final String QUOTE = "\"\'";
+    //private static final String SLASH = "/\\";
+    //private static final String LBRAC = "([{<";
+    //private static final String RBRAC = ">}])";
+    //private static final String SYM = "@#$%^&";
     private static final String ORDER = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.,!?([{<>}])@#$%^&";
     
     public static String encode(String message)
@@ -17,7 +19,14 @@ public class Cryptogram
         
         for (int i = 0; i < message.length(); i++)
         {
-            code += LCASE.charAt(LCASE.indexOf(message.charAt(i)) + 1);
+            if (message.charAt(i) == ' ')
+            {
+                code += " ";
+            }
+            else
+            {
+                code += ORDER.charAt(ORDER.indexOf(message.charAt(i))+1);
+            }
         }
         
         return code;
@@ -27,9 +36,16 @@ public class Cryptogram
     {
         String message = "";
         
-        for (int i = 0; i < message.length(); i++)
+        for (int i = 0; i < code.length(); i++)
         {
-            code += LCASE.charAt(LCASE.indexOf(message.charAt(i)) - 1);
+            if (code.charAt(i) == ' ')
+            {
+                message += " ";
+            }
+            else
+            {
+                message += ORDER.charAt(ORDER.indexOf(message.charAt(i))-1);
+            }
         }
         
         return message;
@@ -38,31 +54,37 @@ public class Cryptogram
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
+        String another = "Y";
         String choice = "";
         
-        while (choice.equals("encode") == false && choice.equals("decode") == false)
+        while (another.equals("Y"))
         {
-            System.out.print("Encode or decode? ");
-            choice = scanner.next().toLowerCase();
-        }
-        
-        scanner.useDelimiter("\n");
-        
-        if (choice.toLowerCase().equals("encode"))
-        {
-            System.out.print("Message to encode: ");
-            String message = scanner.next();
-            System.out.println(Cryptogram.encode(message));
-        }
-        else if (choice.toLowerCase().equals("decode"))
-        {
-            String code = "";
-            System.out.print("Code to decode: ");
-            while (scanner.hasNext())
+            while (choice.equals("encode") == false && choice.equals("decode") == false)
             {
-                code += scanner.next();
+                System.out.print("\nWould you like to encode a message or decode a code? (encode/decode): ");
+                choice = scanner.nextLine().toLowerCase();
             }
-            System.out.println(Cryptogram.decode(code));
+            
+            if (choice.toLowerCase().equals("encode"))
+            {
+                System.out.print("\nMessage to encode: ");
+                String message = scanner.nextLine();
+                System.out.println("Code: " + Cryptogram.encode(message));
+            }
+            else if (choice.toLowerCase().equals("decode"))
+            {
+                System.out.print("\nCode to decode: ");
+                String code = scanner.nextLine();
+                System.out.println("Message: " + Cryptogram.decode(code));
+            }
+            
+            another = "";
+            choice = "";
+            while (another.equals("Y") == false && another.equals("N") == false)
+            {
+                System.out.print("\nWould you like to encode / decode another string? (Y/N): ");
+                another = scanner.nextLine().toUpperCase();
+            }
         }
     }
 }
